d01880e93f4d8a7026ea20e335b0153b058aba67
viktoriazaharova <zaharova181114@gmail.com>
2020-07-22 22:07:28 +0300

    desktop

--- app/js/common.js
+++ app/js/common.js
@@ -4,3 +4,39 @@ $("[name=file]").change(function(){
     var filename = $(this).val().replace(/.*\\/, "");
     $(".file__upload_result").html(filename);
 });
+
+// модальные окна (несколько)
+$(document).ready(function () {
+    var overlay = $('.overlay');
+    var open_modal = $('.open_modal');
+    var close = $('.modal__close, .overlay');
+    var modal = $('.modal__div');
+
+    open_modal.click(function (event) {
+        event.preventDefault();
+        var div = $(this).attr('href');
+        overlay.fadeIn(400,
+            function () {
+                $(div)
+                    .css('display', 'flex')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    });
+
+    close.click(function () {
+        modal
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    overlay.fadeOut(400);
+                }
+            );
+    });
+});
+//end
