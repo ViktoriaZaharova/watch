3270a1d7a51e33ddf4d6d513ec8b37df98268798
viktoriazaharova <zaharova181114@gmail.com>
2020-07-22 20:28:23 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,6 @@
+$('[name=phone]').mask("+7 (999) 999-99-99");
+
+$("[name=file]").change(function(){
+    var filename = $(this).val().replace(/.*\\/, "");
+    $(".file__upload_result").html(filename);
+});
